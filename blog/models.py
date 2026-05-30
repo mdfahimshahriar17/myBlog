cfb28ddc8e644a842cc88b62af40b6f62706ee24
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-05-30 12:13:55 +0600

    Created commdent model

--- blog/models.py
+++ blog/models.py
@@ -31,4 +31,9 @@ class Post(models.Model):
          return self.title
     
 
-    
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
